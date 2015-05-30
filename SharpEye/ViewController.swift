4b7e5aa371f56eabbdcf1f2702ccd707859538f3
Ankur Patel <ankurpatel@Ankurs-MacBook-Pro.local>
2015-05-29 22:27:07 -0500

    Initial Commit

---
+++ SharpEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SharpEye
+//
+//  Created by Ankur Patel on 5/29/15.
+//  Copyright (c) 2015 Ankur Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
